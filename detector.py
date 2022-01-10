28951c6709962871eafd4002aacf12608b382748
Alexandru Cristian Mihaescu <mihaescuac@gmail.com>
2022-01-10 21:36:17 +0200

    Test

--- detector.py
+++ detector.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 
 FACE_CASCADE_PATH = './data/haarcascade_frontalcatface_extended.xml'
 EYE_CASCADE_PATH = './data/haarcascade_eye.xml'
@@ -21,11 +21,44 @@ def detect(img):
         eyes = eye_detector.detectMultiScale(
             face_crop, scaleFactor=1.1, minNeighbors=10,
             minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-        
 
         all_eyes += [(fx+x, fy+y,w, h) for x, y, w, h in eyes]
         
     return faces, all_eyes
+
+
+def detect_pupil(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # _, thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+    # contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    c = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 2, img.shape[0]/2)
+    
+    print(c)
+    pupils = []
+    # for l in c:
+    #     for circle in l:
+    #         center = (circle[0], circle[1])
+    #         radius = circle[2]
+    #         pupils.append(center[0], center[1], radius)
+    
+    return pupils
+    
+def detect_iris(img, pupil):
+    _, t = cv2.threshold(img, 195, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(t, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Find the iris using the radius of the pupil as input.
+    c = cv2.HoughCircles(contours, cv2.HOUGH_GRADIENT, 2, pupil[2] * 2, param2 = 150)
+
+    for l in c:
+        for circle in l:
+            center = (pupil[0], pupil[1])
+            radius = circle[2]
+            # This creates a black image and draws an iris-sized white circle in it.
+            mask = np.zeros((img.shape[0], img.shape[1], 1), np.uint8)
+            cv2.circle(mask, center, radius, (255, 255, 255), thickness = -1)
+            # Mask the iris and crop everything outside of its radius.
+            img = cv2.bitwise_and(img, mask)
+
 
 
 if __name__ == '__main__':
@@ -43,8 +76,13 @@ if __name__ == '__main__':
 
         for (x, y, w, h) in eyes:
             cv2.rectangle(img, (x, y), (x + w, y + h),
-                          (0, 0, 255), 2)   
-        
+                          (0, 0, 255), 2)
+             
+            crop = img[y:y+h, x:x+w]
+            pupils = detect_pupil(crop)
+            
+            for px, py, pr in pupils:
+                cv2.circle(img, (x+px, y+py), pr, (255, 128, 128), 1, cv2.LINE_AA)
 
         cv2.imshow('Test', img)
 
--- gui.py
+++ gui.py
@@ -5,12 +5,9 @@ from PIL import Image, ImageTk
 from baza_date import incarca_bd_mock
 import numpy as np
 
-baza_date = {}
-
-
-def selectare_nume():
+def selectare_nume(bd):
     layout = [[sg.Button(f'Adauga: {nume}', size=(25, 2)), sg.Button(
-        f'Sterge: {nume}', button_color='Red', size=(25, 2))] for nume in baza_date.keys()]
+        f'Sterge: {nume}', button_color='Red', size=(25, 2))] for nume in bd.keys()]
     layout += [[sg.InputText()], [sg.Button('Adaugare nou')]]
     window = sg.Window('Selectare Nume', layout)
 
@@ -26,24 +23,24 @@ def selectare_nume():
             if values[0] == '':
                 sg.Popup('Campul este gol. Introduceti un nume',
                          keep_on_top=True)
-            elif values[0] in baza_date.keys():
+            elif values[0] in bd.keys():
                 sg.Popup('Numele se afla deja in baza de date',
                          keep_on_top=True)
             else:
-                baza_date[values[0]] = []
+                bd[values[0]] = []
                 nume = values[0]
                 break
-        elif event.split()[1] in baza_date.keys():
-            nume = event
+        elif event.split()[1] in bd.keys():
+            del bd[event.split()[1]]
             break
 
     window.close()
 
-    return nume
+    return nume, bd
 
 
-def achizitie_date():
-    nume = selectare_nume()
+def achizitie_date(bd):
+    nume, bd = selectare_nume(bd)
 
     if nume == '':
         return
@@ -63,20 +60,30 @@ def achizitie_date():
         cv2.imshow('Frame', frame)
         key = cv2.waitKey(1)
         if key == 32:
-
-            baza_date[nume].append(frame)
-            print(f'Cadru salvat. Cadre salvate: {len(baza_date[nume])}')
+            bd[nume].append(frame)
+            print(f'Cadru salvat. Cadre salvate: {len(bd[nume])}')
         elif key == 27:
             cv2.destroyAllWindows()
             break
 
 
-def afisare_baza_date(baza_date: Dict):
-    nume = list(baza_date.keys())
+def imagini_de_afisat(bd, nume, pagina):
+    blank = np.zeros((300, 300, 3), dtype=np.uint8)
+
+    l_imagini = len(bd[nume])
+    imagini = [Image.fromarray(bd[nume][i][:, :, ::-1]) for i in range(pagina*4, min((pagina+1)*4, l_imagini))]
+    imagini = [img.resize((300, 300), resample=Image.BICUBIC) for img in imagini]
+    imagini = [ImageTk.PhotoImage(img) for img in imagini]
+    if len(imagini) < 4:
+        imagini += [ImageTk.PhotoImage(Image.fromarray(blank))] * (4-len(imagini))
+        
+    return imagini
+
+
+def afisare_baza_date(bd: Dict):
+    nume = list(bd.keys())
     idx, pagina = 0, 0
     l_nume = len(nume)
-
-    blank = np.zeros((300, 300, 3))
 
     layout = [
         [sg.Image(size=(300, 300), key='i_0'), sg.Image(size=(300, 300), key='i_1')],
@@ -85,24 +92,43 @@ def afisare_baza_date(baza_date: Dict):
         [sg.Button('Utilizatorul Precedent', size=(25, 2)), sg.Button('Utilizatorul Urmator', size=(25, 2))],
     ]
 
-    nume_curent = nume[idx]
-    l_imagini = len(baza_date[nume_curent])
-    imagini = [Image.fromarray(baza_date[nume_curent][i]) for i in range(pagina*4, min((pagina+1)*4), l_imagini)]
-    imagini = [img.resize(300, 300) for img in imagini]
-    imagini = [ImageTk.PhotoImage(img) for img in imagini]
-    if len(imagini) < 4:
-        imagini += [Image.fromarray(blank)] * (4-len(imagini))
+    window = sg.Window('Baza de date', layout, finalize=True)
 
-    window = sg.Window('Baza de date', layout)
+    nume_curent = nume[idx]
+    imagini = imagini_de_afisat(bd, nume_curent, pagina)
 
     for i in range(4):
-        window[f'i_{i}'] = imagini[i]
+        window[f'i_{i}'].update(data=imagini[i])
 
     while True:
         event, values = window.read()
     
         if event in (sg.WIN_CLOSED, 'Cancel'):
             break
+        if event == 'Pagina Precedenta':
+            pagina = (pagina-1) % int(len(bd[nume_curent])/4 + 1) 
+            imagini = imagini_de_afisat(bd, nume_curent, pagina)
+            for i in range(4):
+                window[f'i_{i}'].update(data=imagini[i])
+        elif event == 'Pagina Urmatoare':
+            pagina = (pagina+1) % int(len(bd[nume_curent])/4 + 1)
+            imagini = imagini_de_afisat(bd, nume_curent, pagina)
+            for i in range(4):
+                window[f'i_{i}'].update(data=imagini[i])
+        elif event == 'Utilizatorul Precedent':
+            pagina = 0
+            idx = (idx-1) % len(nume)
+            nume_curent = nume[idx]
+            imagini = imagini_de_afisat(bd, nume_curent, pagina)
+            for i in range(4):
+                window[f'i_{i}'].update(data=imagini[i])
+        elif event == 'Utilizatorul Urmator':
+            pagina = 0
+            idx = (idx+1) % len(nume)
+            nume_curent = nume[idx]
+            imagini = imagini_de_afisat(bd, nume_curent, pagina)
+            for i in range(4):
+                window[f'i_{i}'].update(data=imagini[i])  
 
     window.close()
 
@@ -124,7 +150,7 @@ def gui():
         if event == 'Vizualizare Baza de Date':
             afisare_baza_date(bd)
         elif event == 'Editare Baza de Date':
-            achizitie_date()
+            bd = achizitie_date(bd)
 
         if event in (sg.WIN_CLOSED, 'Cancel'):
             break
